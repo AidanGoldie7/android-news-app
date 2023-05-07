7c91eca5a5ea9fc8e2fa84cc080d278ae4664379
aidangoldie7 <40402670@live.napier.ac.uk>
2023-05-07 22:29:37 +0100

    5. Create java classes for fragments with attached view

---
+++ app/src/main/java/com/example/android_news_app/TechnologyFragment.java
@@ -0,0 +1,22 @@
+package com.example.android_news_app;
+
+//must use same import statement for all fragments or error will occur
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class TechnologyFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //attach fragment
+        View v=inflater.inflate(R.layout.technologyfragment, null);
+        return v;
+    }
+}
