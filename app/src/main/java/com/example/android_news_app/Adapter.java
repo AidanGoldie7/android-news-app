de7daa8f4b42269ddb5e1cd41719ff15431d5ca1
aidangoldie7 <40402670@live.napier.ac.uk>
2023-05-16 21:33:13 +0100

    12. Import Adapter.ViewHolder

--- app/src/main/java/com/example/android_news_app/Adapter.java
+++ app/src/main/java/com/example/android_news_app/Adapter.java
@@ -1,4 +1,27 @@
 package com.example.android_news_app;
 
-public class Adapter {
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.jetbrains.annotations.NotNull;
+
+public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
+    @NonNull
+    @NotNull
+    @Override
+    public Adapter.ViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull @NotNull Adapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 }
