a7af6427846c810d20027bb8f199f462d3bcce02
aidangoldie7 <40402670@live.napier.ac.uk>
2023-05-21 20:32:59 +0100

    17. Retrieve data from API for TechFragment

--- app/src/main/java/com/example/android_news_app/TechnologyFragment.java
+++ app/src/main/java/com/example/android_news_app/TechnologyFragment.java
@@ -9,14 +9,61 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class TechnologyFragment extends Fragment {
+
+    String api ="avd_QiErAGMcEjSocycXS-3DqQl8XVVOmzffj6POpAE";
+    ArrayList<ModelClass> modelClassArrayList;
+    Adapter adapter;
+    String country = "in";
+    private RecyclerView recyclerViewoftech;
+    private String category = "technology";
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         //attach fragment
         View v=inflater.inflate(R.layout.technologyfragment, null);
+
+
+        recyclerViewoftech =v.findViewById(R.id.recyclerviewoftechnology);
+        modelClassArrayList = new ArrayList<>();
+        recyclerViewoftech.setLayoutManager(new LinearLayoutManager(getContext()));
+        adapter=new Adapter(getContext(), modelClassArrayList);
+        recyclerViewoftech.setAdapter(adapter);
+
+        findNews();
+
+
         return v;
+    }
+
+    //fetching data from API
+    private void findNews() {
+
+        ApiUtilities.getApiInterface().getCategoryNews(country, category,100,api).enqueue(new Callback<mainNews>() {
+            @Override
+            public void onResponse(Call<mainNews> call, Response<mainNews> response) {
+                if(response.isSuccessful())
+                {
+                    modelClassArrayList.addAll(response.body().getArticles());
+                    adapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<mainNews> call, Throwable t) {
+
+            }
+        });
+
     }
 }
