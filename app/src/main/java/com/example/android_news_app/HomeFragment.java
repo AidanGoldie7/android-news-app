c6cf789d35044badb13aea275cbd6aae5c8be293
aidangoldie7 <40402670@live.napier.ac.uk>
2023-05-28 20:32:43 +0100

    23. Comments

--- app/src/main/java/com/example/android_news_app/HomeFragment.java
+++ app/src/main/java/com/example/android_news_app/HomeFragment.java
@@ -22,6 +22,7 @@ import retrofit2.Response;
 
 public class HomeFragment extends Fragment {
 
+    //variable declaration
     String api ="avd_QiErAGMcEjSocycXS-3DqQl8XVVOmzffj6POpAE";
     ArrayList<ModelClass> modelClassArrayList;
     Adapter adapter;
