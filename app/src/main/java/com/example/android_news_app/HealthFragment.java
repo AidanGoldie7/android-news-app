fbba1971488429f607e978d9923d686458706057
aidangoldie7 <40402670@live.napier.ac.uk>
2023-05-29 23:48:50 +0100

    24. Comments

--- app/src/main/java/com/example/android_news_app/HealthFragment.java
+++ app/src/main/java/com/example/android_news_app/HealthFragment.java
@@ -20,6 +20,7 @@ import retrofit2.Response;
 
 public class HealthFragment extends Fragment {
 
+    //variable declaration
     String api ="avd_QiErAGMcEjSocycXS-3DqQl8XVVOmzffj6POpAE";
     ArrayList<ModelClass> modelClassArrayList;
     Adapter adapter;
